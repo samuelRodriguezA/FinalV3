4bde640410b2cf68cd42d7bd673106b194c66a61
samuelRodriguezA <srodriguezarmesto@gmail.com>
2023-12-08 14:20:32 -0500

    Server site

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ console.info('Server started on port %s.', config.port)
 })*/
 
 import config from './config/config.js' 
-import app from './server/express.js'
+import app from './express.js'
 import mongoose from 'mongoose' 
 mongoose.Promise = global.Promise
 mongoose.connect(config.mongoUri, { useNewUrlParser: true,
